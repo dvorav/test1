6764bf61968878190edeeee08633bc42e912fbc8
Dillon <d.vorav612@gmail.com>
2021-03-20 09:19:10 -0500

    t

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactjob", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // Start the API server
