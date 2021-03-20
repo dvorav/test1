8cc12dfb86ffff4ccc42b365020408893d516b42
Dillon <d.vorav612@gmail.com>
2021-03-20 09:15:35 -0500

    git push

--- server.js
+++ server.js
@@ -11,16 +11,13 @@ app.use(morgan("dev"));
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-app.use(routes);
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactjob", {
   useNewUrlParser: true,
